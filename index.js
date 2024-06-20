96267aec1639e99060e7a12c68ddb1c0feba5d16
mdaftab41 <mdaftabalam49041@gmail.com>
2024-06-21 02:56:42 +0530

    Adding .env file

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello")
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/contact' ,(req ,res)=>{
+    res.send("Welcome to contact page")
+})
+app.get('/login' ,(req,res)=>{
+    res.send('  <h>Welcome from login page </h><h>please enter correct email and password</h> </div>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
